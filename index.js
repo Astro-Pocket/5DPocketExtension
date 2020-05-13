801bf80e934def4a1793bed067888219a8314d6b
Aweiisacat <awei@LAPTOP-14H3JOK8.localdomain>
2020-05-13 18:52:56 +0800

    做好判斷，API還沒做好

--- index.js
+++ index.js
@@ -1,9 +1,89 @@
 console.log('ready')
+const form = document.querySelector('form')
 const account = document.querySelector('#account')
 const password = document.querySelector('#password')
-const form = document.querySelector('form')
-form.addEventListener('submit',function(e){
-  console.log(password.value)
-  console.log(account.value)
-  return { email: account.value, password: password.value}
+
+//初始化 localStorage 如果登入就不要初始化，有登出就初始化
+function initialize(){
+  if (localStorage.getItem('key') === "undefined" || 
+    localStorage.getItem('key') === null ) 
+  {
+    ToggleLoginUI(false);
+  } else {
+    ToggleLoginUI(true);
+  }
+}
+initialize()
+
+//抓帳號密碼的值
+function getLoginFormValue(){
+  return {email: account.value, password: password.value}
+}
+//修改fetch預設方法
+function postData(url, data){
+  return fetch (url, {
+    cache: 'no-cache',
+    method: 'POST',
+    redirect: 'follow',
+    referrer: 'no-referrer',
+  })
+  .then(res => res.json()) //輸出成json
+}
+//進行登入
+form.addEventListener('submit', function LoginJson(e){
+  e.preventDefault();
+  postData('localhost:3000/api/v1/login', getLoginFormValue())
+  .then(
+    function(data){
+      localStorage.setItem('key', data['auth_token']);
+      if (data['message'] === "ok"){
+        ToggleLoginUI(true);
+      }
+      alert('觀迎登入5DPocket');
+    }
+  )
+  .catch( error => {
+    alert('帳號密碼錯誤!');
+  })
 })
+
+//ToggleLoginUI
+function ToggleLoginUI(isLogin) {
+  let admin = document.getElementById('admin');
+  let frame = document.getElementById('frame');
+  if (isLogin) {
+    admin.classList.add("displaynone");
+    frame.classList.remove("displaynone");
+  }
+  else {
+    localStorage.clear();
+    admin.classList.remove("displaynone");
+    frame.classList.add("displaynone");
+  }
+}
+//登出功能
+let logout = document.getElementById('logout');
+logout.addEventListener('click', e => {
+  logoutjson(e);
+})
+//LogoutTokenData
+function getLogoutFormValues(){
+  var logouttoken = localStorage.getItem('key');
+  return { auth_token: logouttoken }
+}
+//LogoutTokenDataApi
+function logoutjson(e){
+  e.preventDefault();
+  postData('localhost:3000/api/v2/login',getLogoutFormValues())
+    .then(
+      function(data){
+        console.log(data);
+        ToggleLoginUI(false);
+      }
+    ) // JSON from `resp.json()` call
+    .catch(
+      error => {
+        console.log(error)
+      }
+    )
+}
