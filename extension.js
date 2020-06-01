85eb341b407d9b94fa6da534150778db7e637a38
Aweiisacat <face992302@gmail.com>
2020-06-01 09:26:12 +0800

    儲存文章以及tag分開做

--- extension.js
+++ extension.js
@@ -1,10 +1,6 @@
-const form = document.querySelector("form");
-const account = document.querySelector("#account");
-const password = document.querySelector("#password");
-const save = document.querySelector("#save");
-
 // 開網頁focus到帳號輸入框上
 document.querySelector("#account").focus();
+
 //初始化 localStorage 如果登入就不要初始化，有登出就初始化
 function initialize() {
   if (
@@ -20,6 +16,8 @@ initialize();
 
 //抓帳號密碼的值
 function getLoginFormValue() {
+  const account = document.querySelector("#account");
+  const password = document.querySelector("#password");
   return { email: account.value, password: password.value };
 }
 //修改fetch預設方法
@@ -38,7 +36,7 @@ function postData(url, data) {
   }).then((res) => res.json()); //輸出成json
 }
 //進行登入
-form.addEventListener("submit", function LoginJson(e) {
+document.querySelector("form").addEventListener("submit", function LoginJson(e) {
   e.preventDefault();
   postData("http://127.0.0.1:3000/api/v1/login", getLoginFormValue())
     .then(function (data) {
@@ -88,28 +86,30 @@ function logoutjson(e) {
       console.log(error);
     });
 }
-// 進行儲存文章以及儲存tags的動作
-save.addEventListener("click", () => {
+// 使用select2
+$(".js-tag-select").select2({
+  multiple: true,
+  tags: true,
+  placeholder: "Add Tags",
+});
+// 進行儲存文章
+document.addEventListener("DOMContentLoaded", () => {
   const spinner = document.querySelector(".spinner-border");
-  const save = document.getElementById('save')
   spinner.classList.remove("displaynone");
-  document.querySelector(".flash").classList.remove("displaynone");
-  save.disabled=true
-  save.innerHTML = '儲存中'
+  const saving = document.querySelector(".saving");
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     const url = tabs[0].url;
     const key = localStorage.getItem("key");
-    const tags = document.querySelector('#settags').value
     // 取得url&key的值
     function geturl() {
-      return { url: url, key: key, tags: tags};
+      return { url: url, key: key };
     }
-    postData("http:127.0.0.1:3000/api/v1/save", geturl())
+    postData("http:127.0.0.1:3000/api/v1/save_article", geturl())
       .then((data) => {
         document.querySelector(".flash").innerHTML = data["message"];
         spinner.classList.add("displaynone");
-        save.disabled=false
-        save.innerHTML = '儲存'
+        saving.classList.add("displaynone");
+        document.querySelector(".save").classList.remove("displaynone");
         setTimeout(function () {
           document.querySelector(".flash").classList.add("displaynone");
         }, 3000);
@@ -119,13 +119,24 @@ save.addEventListener("click", () => {
       });
   });
 });
-// 使用select2
-$(".js-tag-select").select2({
-  multiple: true,
-  tags: true,
-  placeholder: "Add Tags",
-  language: {
-    noResults: function (params) {
-    return "沒有標籤";
-  }}
+// 進行儲存tags的動作
+document.querySelector(".save").addEventListener("submit", () => {
+  chrome.tabs.query({ active: true }, (tabs) => {
+    const url = tabs[0].url;
+    const key = localStorage.getItem("key");
+    const tags = $("#settags").val();
+    console.log(tags);
+    //取得url&key&tag
+    function getdata() {
+      return { url: url, key: key, tags: tags };
+    }
+    postData("http:127.0.0.1:3000/api/v1/save_tags", getdata()).then((data) => {
+      document.querySelector(".flash").innerHTML = data["message"];
+    });
+  });
 });
+
+//進行搜尋是否有沒有這篇文章的動作
+document.addEventListener("DOMContentLoaded", () => {
+  
+})
