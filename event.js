b1f0fb40f2b901a184d1716bb12cd5052b033fb2
Aweiisacat <face992302@gmail.com>
2020-06-05 11:29:48 +0800

    manifest setting

--- event.js
+++ event.js
@@ -12,7 +12,7 @@ chrome.contextMenus.create({
 window.menuData = {
   url: null,
   key: null,
-  host: 'https://five-dimension-pocket.herokuapp.com/api/v1/',
+  host: 'https://127.0.0.1:3000/api/v1/',
   save: 'save_article'
 }
 
--- extension.js
+++ extension.js
@@ -11,7 +11,7 @@ window.PocketData = {
     login: 'login',
     logout: 'logout'
   },
-  host: 'https://five-dimension-pocket.herokuapp.com/api/v1/',
+  host: 'https://127.0.0.1:3000/api/v1/',
   tags: null,
   email: null,
   password: null,
